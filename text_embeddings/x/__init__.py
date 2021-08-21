ccb258225d689d9358d2095d0372c616d30f924e
Chenghao Mou <mouchenghao@gmail.com>
2021-08-20 20:11:31 -0700

    fix x bugs

--- text_embeddings/x/__init__.py
+++ text_embeddings/x/__init__.py
@@ -13,8 +13,8 @@ import torch.nn as nn
 from torch import Tensor
 from einops import repeat, rearrange
 
-class AttentionWrapper(nn.Module):
 
+class AttentionWrapper(nn.Module):
     def __init__(
         self,
         attention_class: Callable,
@@ -26,10 +26,12 @@ class AttentionWrapper(nn.Module):
         is_cross_attention: bool,
     ):
         super().__init__()
-        
+
         self.is_cross_attention = is_cross_attention
         self.pre_attention_q_norm = nn.LayerNorm(embed_dim)
-        self.pre_attention_kv_norm = nn.LayerNorm(embed_dim) if is_cross_attention else None
+        self.pre_attention_kv_norm = (
+            nn.LayerNorm(embed_dim) if is_cross_attention else None
+        )
 
         self.attention = attention_class(
             embed_dim=embed_dim,
@@ -38,32 +40,44 @@ class AttentionWrapper(nn.Module):
             batch_first=batch_first,
         )
         self.ff = nn.Sequential(
-            nn.Linear(embed_dim, embed_dim * ff_dim),
+            nn.Linear(embed_dim, ff_dim),
             nn.GELU(),
             nn.Dropout(dropout),
-            nn.Linear(embed_dim * ff_dim, embed_dim)
+            nn.Linear(ff_dim, embed_dim),
         )
-    
+
     def forward(
-        self, 
-        query: Tensor, 
-        key: Tensor = None, 
-        value: Tensor = None, 
+        self,
+        query: Tensor,
+        key: Tensor = None,
+        value: Tensor = None,
         mask: Tensor = None,
     ):
         query = self.pre_attention_q_norm(query)
-        key = self.pre_attention_kv_norm(key) if key is not None and self.pre_attention_kv_norm is not None else key
-        value = self.pre_attention_kv_norm(value) if value is not None and self.pre_attention_kv_norm is not None else value
-    
+        key = (
+            self.pre_attention_kv_norm(key)
+            if key is not None and self.pre_attention_kv_norm is not None
+            else key
+        )
+        value = (
+            self.pre_attention_kv_norm(value)
+            if value is not None and self.pre_attention_kv_norm is not None
+            else value
+        )
+
         # mask is only useful for cross attention, ignore attention weights
-        attn_output, *_ = self.attention(query, key, value, key_padding_mask=mask) if self.is_cross_attention else self.attention(query, query, query)
+        attn_output, *_ = (
+            self.attention(query, key, value, key_padding_mask=mask)
+            if self.is_cross_attention
+            else self.attention(query, query, query)
+        )
         output = attn_output + query
         output = self.ff(output) + output
 
         return output
 
-class XLayer(nn.Module):
 
+class XLayer(nn.Module):
     def __init__(
         self,
         embed_dim: int,
@@ -88,23 +102,26 @@ class XLayer(nn.Module):
         )
 
         # pesudo transfomer
-        self.latent_attentions = nn.ModuleList([
-            AttentionWrapper(
-                attention_class=latent_attention,
-                embed_dim=embed_dim,
-                num_heads=num_latent_attention_heads,
-                ff_dim=ff_dim,
-                dropout=dropout,
-                batch_first=batch_first,
-                is_cross_attention=False,
-            ) for _ in range(num_latent_layers)
-        ])
-    
+        self.latent_attentions = nn.ModuleList(
+            [
+                AttentionWrapper(
+                    attention_class=latent_attention,
+                    embed_dim=embed_dim,
+                    num_heads=num_latent_attention_heads,
+                    ff_dim=ff_dim,
+                    dropout=dropout,
+                    batch_first=batch_first,
+                    is_cross_attention=False,
+                )
+                for _ in range(num_latent_layers)
+            ]
+        )
+
     def forward(
         self,
-        query: Tensor, 
-        key: Tensor = None, 
-        value: Tensor = None, 
+        query: Tensor,
+        key: Tensor = None,
+        value: Tensor = None,
         mask: Tensor = None,
     ):
         o = self.cross_attention(
@@ -116,11 +133,11 @@ class XLayer(nn.Module):
 
         for attn in self.latent_attentions:
             o = attn(o)
-        
+
         return o
 
-class X(nn.Module):
 
+class X(nn.Module):
     def __init__(
         self,
         num_classes: int,
@@ -138,26 +155,26 @@ class X(nn.Module):
         super().__init__()
 
         self.embedding = nn.Embedding(256 + 1, embed_dim, padding_idx=0)
-        self.layers = nn.ModuleList([
-            XLayer(
-                embed_dim=embed_dim,
-                num_cross_attention_heads=num_cross_attention_heads,
-                num_latent_attention_heads=num_latent_attention_heads,
-                num_latent_layers=num_latent_layers,
-                ff_dim=ff_dim,
-                dropout=dropout,
-                batch_first=batch_first,
-                latent_attention=latent_attention
-            ) for _ in range(num_layers)
-        ])
+        self.layers = nn.ModuleList(
+            [
+                XLayer(
+                    embed_dim=embed_dim,
+                    num_cross_attention_heads=num_cross_attention_heads,
+                    num_latent_attention_heads=num_latent_attention_heads,
+                    num_latent_layers=num_latent_layers,
+                    ff_dim=ff_dim,
+                    dropout=dropout,
+                    batch_first=batch_first,
+                    latent_attention=latent_attention,
+                )
+                for _ in range(num_layers)
+            ]
+        )
 
         self.num_classes = num_classes
         self.latent = nn.Parameter(torch.rand((latent_dim, embed_dim)))
         self.output_layer = nn.Linear(embed_dim, self.num_classes)
-        self.lambda_layer = nn.Sequential(
-            nn.Linear(embed_dim, 1),
-            nn.Sigmoid()
-        )
+        self.lambda_layer = nn.Sequential(nn.Linear(embed_dim, 1), nn.Sigmoid())
 
     def forward(
         self,
@@ -170,7 +187,9 @@ class X(nn.Module):
         un_halted_prob = x.new_ones((batch_size,))
         halted = x.new_zeros((batch_size,))
 
-        latent = repeat(rearrange(self.latent, "N D -> 1 N D"), "1 N D -> B N D", B = batch_size)
+        latent = repeat(
+            rearrange(self.latent, "N D -> 1 N D"), "1 N D -> B N D", B=batch_size
+        )
 
         probas = []
         preds = []
@@ -179,123 +198,84 @@ class X(nn.Module):
         y_m = x.new_zeros((batch_size, self.num_classes))
 
         for i, layer in enumerate(self.layers):
-            latent = layer(
-                latent,
-                x,
-                x,
-                mask
-            )
-            
+            latent = layer(latent, x, x, mask)
+
             # calculate halting probability for current layer
-            layer_lambda = x.new_ones((batch_size,)) if i == len(self.layers) - 1 else self.lambda_layer(torch.mean(latent, dim=1))
-            # calculate current prediction from current layer
-            layer_predictions = self.output_layer(
-                torch.mean(latent, dim=1)
+            layer_lambda = (
+                x.new_ones((batch_size,))
+                if i == len(self.layers) - 1
+                else self.lambda_layer(torch.mean(latent, dim=1))
             )
+            # calculate current prediction from current layer
+            layer_predictions = self.output_layer(torch.mean(latent, dim=1))
 
             # conditional halting probability for current layer: previously not halted * halting now
             layer_halted_prob = un_halted_prob * layer_lambda.view(-1)
             un_halted_prob = un_halted_prob * (1 - layer_lambda.view(-1))
-            
+
             # Halt based on the halting probability
-            halt = torch.bernoulli(layer_lambda.view(-1)) * (1 - halted)
-            
+            sampling = torch.bernoulli(layer_lambda.reshape(-1))
+            halt = sampling * (1 - halted)
+
             probas.append(layer_halted_prob)
             preds.append(layer_predictions)
 
             p_m = p_m * (1 - halt) + layer_halted_prob * halt
-            
-            y_m = y_m * repeat(1 - halt, "B -> B C", C=self.num_classes) + layer_predictions * repeat(halt, "B -> B C", C=self.num_classes)
+
+            y_m = y_m * repeat(
+                1 - halt, "B -> B C", C=self.num_classes
+            ) + layer_predictions * repeat(halt, "B -> B C", C=self.num_classes)
 
             halted = halted + halt
 
             if not self.training and halted.sum() == batch_size:
                 break
-        
+
         return torch.stack(probas), torch.stack(preds), p_m, y_m
 
-class ReconstructionLoss(nn.Module):
 
+class ReconstructionLoss(nn.Module):
     def __init__(self, loss_fn: Callable):
         super().__init__()
         self.loss_fn = loss_fn
-    
+
     def forward(self, probas, preds, labels):
 
-        total = preds.new_tensor(0.)
+        total = preds.new_tensor(0.0)
         for layer_probas, layer_preds in zip(probas, preds):
             layer_loss = layer_probas * self.loss_fn(layer_preds, labels)
             total = total + layer_loss.mean()
-        
+
         return total
 
-class RegularizationLoss(nn.Module):
 
+class RegularizationLoss(nn.Module):
     def __init__(self, lambda_p: float, max_layers: int):
         super().__init__()
         p_g = torch.zeros((max_layers,))
-        not_halted = 1.
+        not_halted = 1.0
         for k in range(max_layers):
             p_g[k] = lambda_p * not_halted
             not_halted = not_halted * (1 - lambda_p)
 
         self.p_g = nn.Parameter(p_g, requires_grad=False)
-        self.kl_div = nn.KLDivLoss(reduction='batchmean')
-    
+        self.kl_div = nn.KLDivLoss(reduction="batchmean")
+
     def forward(self, probas):
         probas = probas.transpose(0, 1)
-        p_g = self.p_g[None, :probas.shape[1]].expand_as(probas)
-        
+        p_g = self.p_g[None, : probas.shape[1]].expand_as(probas)
+
         return self.kl_div(probas.log(), p_g)
 
-class XLoss(nn.Module):
 
+class XLoss(nn.Module):
     def __init__(self, loss_fn: Callable, lambda_p: float, max_layers: int):
         super().__init__()
         self.reconstruction_loss = ReconstructionLoss(loss_fn)
         self.regularization_loss = RegularizationLoss(lambda_p, max_layers)
-    
+
     def forward(self, probas, preds, labels):
 
-        return self.reconstruction_loss(probas, preds, labels) + self.regularization_loss(probas)
-
-
-if __name__ == "__main__":
-
-    from torchsummary import summary
-
-    embed_dim = 64
-    seq_length = 2048
-
-    model = X(
-        num_classes=2,
-        latent_dim=256,
-        num_layers=12,
-        embed_dim=embed_dim,
-        num_cross_attention_heads=1,
-        num_latent_attention_heads=2,
-        num_latent_layers=2,
-        ff_dim=embed_dim * 2,
-        dropout=0.1,
-        batch_first=True,
-        latent_attention=nn.MultiheadAttention,
-    )
-
-    loss = XLoss(nn.CrossEntropyLoss(reduction='mean'), lambda_p=0.9, max_layers=12)
-
-    # help(summary)
-    summary(model, input_size=(seq_length, embed_dim), batch_size=10, device='cpu')
-
-    x = torch.randn(10, seq_length, embed_dim)
-    mask = torch.rand(10, seq_length)
-    mask = mask <= 0.5
-    
-    probas, preds, final_proba, final_pred = model(
-        x,
-        mask
-    )
-    
-    l = loss(probas, preds, torch.ones((10,)).long())
-    print(l)
-
-    
+        return self.reconstruction_loss(
+            probas, preds, labels
+        ) + self.regularization_loss(probas)
